9356aa9e9c0a0f91f05d42038fccf805de888b65
philiplee25 <yplunap25@gmail.com>
2021-01-26 20:48:28 +0900

    .

--- app/src/main/java/com/eomcs/project/App.java
+++ app/src/main/java/com/eomcs/project/App.java
@@ -10,7 +10,7 @@ public class App {
   public static void main(String[] args) {
 
     loop: while (true) {
-      System.out.println("/신체정보 추가");
+      System.out.println("/신체정보 등록");
       System.out.println("/신체정보 조회");
       System.out.println("/운동기록 추가");
       System.out.println("/운동기록 조회");
@@ -22,7 +22,7 @@ public class App {
 
       switch (command) {
 
-        case "/신체정보 추가":
+        case "/신체정보 등록":
           BodyHandler.add();
           break;
 
@@ -30,19 +30,19 @@ public class App {
           BodyHandler.list();
           break;
 
-        case "/운동기록 추가":
+        case "/운동 추가하기":
           ExerciseHandler.add();
           break;
 
-        case "/운동기록 조회":
+        case "/운동 조회하기":
           ExerciseHandler.list();
           break;
 
-        case "/식단 등록":
+        case "/식사 등록":
           DietHandler.add();
           break;
 
-        case "/식단 조회":
+        case "/식사 조회":
           DietHandler.list();
           break;
 
